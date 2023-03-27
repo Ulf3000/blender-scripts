f749cc4d9e96f506353051f9d54fce48340f2de3
Alex Mehler <alexmehler@t-online.de>
2023-03-27 23:19:26 +0200

    Add files via upload
    add msgbus callback for mode change and active object change

--- sort_npanels.py
+++ sort_npanels.py
@@ -5,26 +5,48 @@ import bpy
 from bpy.app.handlers import persistent   # the script stay active even if new blend file is loaded
 
 @persistent
-def update_sidebar(context):
-    print("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ",context.workspace.name)
+def update_sidebar():
+    print("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ")
     bpy.ops.simpletabs.refresh() # refresh simple tabs addon
     bpy.ops.simpletabs.update()
 
 @persistent
 def scene_loaded_handler(scene):
+    
     # create generic object() as handle
     handle = object()
+    
     # subscribe to the workspace change event
-    subscribe_to = bpy.types.Window, "workspace"
+    workspace_subscribe_to = bpy.types.Window, "workspace"
     bpy.msgbus.subscribe_rna(
-        key=subscribe_to,
+        key=workspace_subscribe_to,
         owner=handle,
-        args=(bpy.context,),
+        args=(),
+        notify=update_sidebar,
+    )
+
+    # subscribe to the active object change event
+    object_subscribe_to = bpy.types.LayerObjects, "active"
+    bpy.msgbus.subscribe_rna(
+        key=object_subscribe_to,
+        owner=handle,
+        args=(),
+        notify=update_sidebar,
+    )
+
+    # subscribe to the mode change event
+    mode_subscribe_to = bpy.types.Object, "mode"
+    bpy.msgbus.subscribe_rna(
+        key=mode_subscribe_to,
+        owner=handle,
+        args=(),
         notify=update_sidebar,
     )
     
     # publish an initial event to trigger the update_sidebar function
-    bpy.msgbus.publish_rna(key=subscribe_to)
+    bpy.msgbus.publish_rna(key=workspace_subscribe_to)
+    bpy.msgbus.publish_rna(key=object_subscribe_to)
+    bpy.msgbus.publish_rna(key=mode_subscribe_to)
 
 def register():
     if not scene_loaded_handler in bpy.app.handlers.load_post:
